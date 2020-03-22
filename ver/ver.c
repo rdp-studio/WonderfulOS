b59218c9c2d73391af10168d2155ea626333d2e5
RDPStudio_Admin-Jack <5127158+RDPStudio-top@user.noreply.gitee.com>
2020-03-22 20:37:17 +0800

    Update CODE

--- ver/ver.c
+++ ver/ver.c
@@ -26,5 +26,26 @@ void HariMain(void)
 	api_putchar('0');
 	api_putchar('0');
 	api_putchar('1');
+	api_putchar(' ');
+                /* DO NOT DELETE 请勿删除版权，否则需承担法律责任。 */
+	api_putchar('P');
+	api_putchar('o');
+	api_putchar('w');
+	api_putchar('e');
+	api_putchar('r');
+	api_putchar('d');
+	api_putchar('');
+	api_putchar('B');
+	api_putchar('y');
+	api_putchar(' ');
+	api_putchar('R');
+	api_putchar('D');
+	api_putchar('P');
+	api_putchar('S');
+	api_putchar('t');
+	api_putchar('u');
+	api_putchar('d');
+	api_putchar('i');
+	api_putchar('o');
 	api_end();
 }
